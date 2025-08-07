85f9b11af5c9187589cd6ce1fb0970a221a1ff31
Tom van Dijk <t.vandijk@utwente.nl>
2025-08-07 22:05:32 +0200

    This should actually be correct

--- src/main.c
+++ src/main.c
@@ -56,16 +56,21 @@ double now_seconds() {
 int writer_thread(void* _) {
     uint64_t val = 1;
     while (running) {
-        atomic_store_explicit(&s_bucket, 0xC0000000, memory_order_relaxed); // mark as locked
+        uint32_t s = atomic_load_explicit(&s_bucket, memory_order_relaxed);
+        if (s & 0xC0000000) continue;
+        if (atomic_compare_exchange_weak(&s_bucket, &s, s|0xC0000000)) {
+            //atomic_store_explicit(&s_bucket, 0xC0000000, memory_order_relaxed);
+            //atomic_thread_fence(memory_order_release);
 
-        bucket.a = val;
-        bucket.b = val * 2;
-        bucket.c = val * 3;
-        bucket.res = val * 6;
+            bucket.a = val;
+            bucket.b = val * 2;
+            bucket.c = val * 3;
+            bucket.res = val * 6;
 
-        // update status with a changing version counter
-        atomic_store_explicit(&s_bucket, (val & 0x3FFFFFFF), memory_order_release);
-        val++;
+            // update status with a changing version counter
+            atomic_store_explicit(&s_bucket, (val & 0x3FFFFFFF), memory_order_release);
+            val++;
+        }
     }
     return 0;
 }
@@ -74,18 +79,22 @@ int reader_thread(void* _) {
     long i, j = 0;
     double start = now_seconds();
     for (i = 0; i < 400000000; ++i) {
-        uint32_t s1 = atomic_load_explicit(&s_bucket, memory_order_acquire);
-        //uint32_t s1 = atomic_load_explicit(&s_bucket, memory_order_relaxed);
-
+        // An acquire operation, all loads after this may not reorder before
+        //uint32_t s1 = atomic_load_explicit(&s_bucket, memory_order_acquire);
+        uint32_t s1 = atomic_load_explicit(&s_bucket, memory_order_relaxed);
         if (s1 & 0xC0000000) continue;
 
+        atomic_thread_fence(memory_order_acquire);
         uint64_t a = bucket.a;
         uint64_t b = bucket.b;
         uint64_t c = bucket.c;
         uint64_t res = bucket.res;
 
-        atomic_thread_fence(memory_order_seq_cst);
+        //atomic_thread_fence(memory_order_seq_cst);
+        //atomic_thread_fence(memory_order_acq_rel);
         //atomic_thread_fence(memory_order_acquire);
+        // Acquire *FENCE* will prevent earlier reads reordered with any following read/write
+        atomic_thread_fence(memory_order_acquire);
         //uint32_t s2 = atomic_load_explicit(&s_bucket, memory_order_acquire);
         //uint32_t s2 = atomic_load_explicit(&s_bucket, memory_order_seq_cst);
         uint32_t s2 = atomic_load_explicit(&s_bucket, memory_order_relaxed);
@@ -105,13 +114,17 @@ int reader_thread(void* _) {
 }
 
 int main() {
-    thrd_t writer, reader;
-    thrd_create(&writer, writer_thread, NULL);
+    thrd_t writer1, writer2, writer3, reader;
+    thrd_create(&writer1, writer_thread, NULL);
+    thrd_create(&writer2, writer_thread, NULL);
+    thrd_create(&writer3, writer_thread, NULL);
     thrd_create(&reader, reader_thread, NULL);
 
     thrd_join(reader, NULL);
     running = 0;
-    thrd_join(writer, NULL);
+    thrd_join(writer1, NULL);
+    thrd_join(writer2, NULL);
+    thrd_join(writer3, NULL);
 
     return 0;
 }
